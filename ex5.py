ba23c3f2b394294c3eff4dcf23459469e3048749
Sagi Rolland <sagi.rolland@gmail.com>
2024-04-05 14:39:04 +0300

    "fin"

--- ex5.py
+++ ex5.py
@@ -15,16 +15,14 @@ class VigenereCipher:
         encriptmessage= ''
         for char in message:
             if char in alphabet:
-                shift = alphabet.index(self.key[keyindex % len(self.key)])
-                encrypted_char = alphabet[(alphabet.index(char) + shift) % len(alphabet)]
-                encriptmessage += encrypted_char
+                shift =( alphabet.index(char)+self.key[keyindex])% len(alphabet)
+                encriptmessage += alphabet[shift]
                 keyindex += 1
                 if keyindex==len(self.key):
                     keyindex=0
             elif char in Capital_alphabet:
-                shift = Capital_alphabet.index(self.key[keyindex % len(self.key)])
-                encrypted_char = Capital_alphabet[(Capital_alphabet.index(char) + shift) % len(Capital_alphabet)]
-                encriptmessage += encrypted_char
+                shift =( Capital_alphabet.index(char)+self.key[keyindex])% len(Capital_alphabet)
+                encriptmessage += Capital_alphabet[shift]
                 keyindex += 1
                 if keyindex==len(self.key):
                     keyindex=0
@@ -33,85 +31,69 @@ class VigenereCipher:
         return encriptmessage
     
     def decrypt(self, message):
-        for number in self.key:
-            self.key[number]= -self.key[number]
-        decoded= self.encrypt(self,message).copy()
+        for i, e in enumerate(self.key):
+            self.key[i]= -e
+        decoded= self.encrypt(message)
         return decoded
     
 class CaesarCipher(VigenereCipher):
     def __init__(self, key):
-        super().__init__(key)
+        arrkey = [key]
+        super().__init__(arrkey)
 
     def encrypt(self, message):
-        encriptmessage= message.copy()
-        encriptmessage=self.encrypt(message,self.key)
+        
+        encriptmessage=super().encrypt(message)
         return encriptmessage
             
     def decrypt(self,message):
-        key = -self.key
-        encriptmessage= message.copy()
-        encriptmessage=self.decrypt(message,key)
+        
+        encriptmessage=super().decrypt(message)
         return encriptmessage
     
     def key_shift(self,delta):
-        self.key = self.key + delta
+        self.key[0] = self.key[0] + delta
             
 def loadEncryptionSystem(dir_path, plaintext_suffix):
-    with open(dir_path,'r') as f:
+    with open(os.path.join(dir_path,"config.json"),'r') as f:
         loaded_dict = json.load(f)
     
     entries = os.listdir(dir_path)
 
     if loaded_dict["type"]=="Vigenere":
-        Vigenere_Cipher=VigenereCipher(Vigenere_Cipher,key=loaded_dict["key"])
-        
-        for entry in entries:
-            entry_path = os.path.join(dir_path, entry)
-            if os.path.isfile(entry_path):
-                shutil.copy2(entry_path, entry_path)
-                if loaded_dict("encrypt")== "True":
-                    if entry.endswith(plaintext_suffix):
-                        os.rename(entry_path, entry_path + ".enc")
-                        with open(entry_path, 'r+') as file:
-                            content = file.read()
-                            encrypted_content = Vigenere_Cipher.encrypt(content)
-                            file.seek(0)
-                            file.write(encrypted_content)
-                            file.truncate()
-
-                if loaded_dict("encrypt")== "False":            
-                    if entry.endswith(".enc"):
-                        os.rename(entry_path, entry_path.replace(".enc", plaintext_suffix))
-                        with open(entry_path, 'r+') as file:
-                            content = file.read()
-                            decrypted_content = Vigenere_Cipher.decrypt(content)
-                            file.seek(0)
-                            file.write(decrypted_content)
-                            file.truncate()
-
+        cipher=VigenereCipher(key=loaded_dict["key"])
     elif loaded_dict["type"]=="Caesar":
-        Caesar_Cipher=CaesarCipher(Caesar_Cipher,key=loaded_dict["key"])
+        cipher=CaesarCipher(key=loaded_dict["key"])
+    
+    for entry in entries:
+        entry_path = os.path.join(dir_path, entry)
+        if os.path.isfile(entry_path):
+            if loaded_dict["encrypt"]== "True":
+                if entry.endswith(plaintext_suffix):
+                    destpath=entry_path.replace(plaintext_suffix,"enc")
+                    shutil.copy2(entry_path, destpath)
+                    
+                    with open(entry_path, 'r+') as source:
+                        content = source.read()
+                        encrypted_content = cipher.encrypt(content)
+                        with open(destpath,"w") as dest:
+                            dest.seek(0)
+                            dest.write(encrypted_content)
+                            dest.truncate()
 
-        for entry in entries:
-            entry_path = os.path.join(dir_path, entry)
-            if os.path.isfile(entry_path):
-                shutil.copy2(entry_path, entry_path)
-                if loaded_dict("encrypt")== "True":
-                    if entry.endswith(plaintext_suffix):
-                        os.rename(entry_path, entry_path + ".enc")
-                        with open(entry_path, 'r+') as file:
-                            content = file.read()
-                            encrypted_content = Caesar_Cipher.encrypt(content)
-                            file.seek(0)
-                            file.write(encrypted_content)
-                            file.truncate()
-                            
-                if loaded_dict("encrypt")== "False":            
-                    if entry.endswith(".enc"):
-                        os.rename(entry_path, entry_path.replace(".enc", plaintext_suffix))
-                        with open(entry_path, 'r+') as file:
-                            content = file.read()
-                            decrypted_content = Caesar_Cipher.decrypt(content)
-                            file.seek(0)
-                            file.write(decrypted_content)
-                            file.truncate()
+            if loaded_dict["encrypt"]== "False":            
+                if entry.endswith(".enc"):
+                    destpath=entry_path.replace("enc",plaintext_suffix)
+                    shutil.copy2(entry_path, destpath)
+                    with open(entry_path, 'r+') as source:
+                        content = source.read()
+                        decrypted_content = cipher.decrypt(content)
+                        with open(destpath,"w") as dest:
+                            dest.seek(0)
+                            dest.write(decrypted_content)
+                            dest.truncate()
+
+if __name__ == '__main__':
+    caesar = CaesarCipher(1)
+    caesar.key_shift(2)
+    print( 'd' == caesar.encrypt('a'))
